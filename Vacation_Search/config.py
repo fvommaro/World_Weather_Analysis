99ce4d2228f298b8469f9f81016b5ae00a51c0fc
fvommaro <108023106+fvommaro@users.noreply.github.com>
2022-08-17 20:06:21 -0700

    Delete config.py

--- Vacation_Search/config.py
+++
@@ -1,8 +0,0 @@
-# New York Times
-NYT_API_KEY = "Q1M1A71PBP77uUKqZ0mlBcZrQLvzgL9t"
-
-# Open Weather Map
-WEATHER_API_KEY = "2f54df0ee9541f7eec426cb0a51c829b"
-
-# Google
-GOOGLE_API_KEY = "AIzaSyDYO_e59wpHuPln8yLBgfmhntd7ZMRQzSc"
